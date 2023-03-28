548656314072891c5deac598a81bc900c4602e95
DESKTOP-EL54GVD\USER <onyebuchiaja@gmail.com>
2023-03-28 15:49:05 +0100

    "create currencyExchangeController and class"

---
+++ src/main/java/controller/CurrencyExchangeController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to
+    ){
+    return new CurrencyExchange(100l, from, to, BigDecimal.valueOf(50));
+    }
+}
